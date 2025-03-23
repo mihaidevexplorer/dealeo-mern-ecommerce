40af4d863be07e0e3ababfe953e36b2c77e8914a
Mihai Tașu <146973331+mihaidevexplorer@users.noreply.github.com>
2025-03-23 14:02:22 +0200

    Update Shipping.jsx

--- frontend/src/pages/Shipping.jsx
+++ frontend/src/pages/Shipping.jsx
@@ -1,248 +1,238 @@
-//src/pages/Shipping.jsx
-import { useState } from 'react';
+//src/pages/Shops.jsx
+import { useState,useEffect } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { IoIosArrowForward } from "react-icons/io";
-import { useDispatch } from 'react-redux';
-import { place_order } from '../store/reducers/orderReducer';
-import { useSelector } from 'react-redux';
+import { Range } from 'react-range';
+import {AiFillStar} from 'react-icons/ai'
+import {CiStar} from 'react-icons/ci' 
+import Products from '../components/products/Products';
+import {BsFillGridFill} from 'react-icons/bs'
+import {FaThList} from 'react-icons/fa'
+import ShopProducts from '../components/products/ShopProducts';
+import Pagination from '../components/Pagination';
+import { useDispatch, useSelector } from 'react-redux';
+import { price_range_product,query_products } from '../store/reducers/homeReducer';
 
+const Shops = () => {
 
-const Shipping = () => {
-
-    const { state: {products,price,shipping_fee,items }} = useLocation()
     const dispatch = useDispatch()
-    const navigate = useNavigate()
-    const {userInfo} = useSelector(state => state.auth) 
+    const {products,categorys,priceRange,latest_product,totalProduct,parPage} = useSelector(state => state.home)
 
-    const [res, setRes] = useState(false)
-    const [state, setState] = useState({
-        name: '',
-        address: '',
-        phone: '',
-        post: '',
-        province: '',
-        city: '',
-        area: ''
-    })
-
-    const inputHandle = (e) => {
+    useEffect(() => { 
+        dispatch(price_range_product())
+    },[dispatch])
+    useEffect(() => { 
         setState({
-            ...state,
-            [e.target.name]: e.target.value
+            values: [priceRange.low, priceRange.high]
         })
-    }
+    },[priceRange])
 
-    const save = (e) => {
-        e.preventDefault()
-        const {name,address,phone,post,province,city,area } = state;
-        if (name && address && phone && post && province && city && area) {
-            setRes(true)
+    const [filter, setFilter] = useState(true) 
+
+    const [state, setState] = useState({values: [priceRange.low, priceRange.high]})
+    const [rating, setRating] = useState('')
+    const [styles, setStyles] = useState('grid')
+
+   
+    const [pageNumber, setPageNumber] = useState(1)
+
+    const [sortPrice, setSortPrice] = useState('')
+    const [category, setCategory] = useState('')
+    const queryCategory = (e, value) => {
+        if (e.target.checked) {
+            setCategory(value)
+        } else {
+            setCategory('')
         }
-
     }
 
-    const placeOrder = () => {
-        dispatch(place_order({
-            price,
-            products,
-            shipping_fee,
-            items,
-            shippingInfo : state,
-            userId: userInfo,
-            navigate 
-
-        }))
-    }
+    useEffect(() => { 
+        dispatch(
+            query_products({
+                low: state.values[0],
+                high: state.values[1],
+                category,
+                rating,
+                sortPrice,
+                pageNumber
+            })
+         )
+    },[state.values,category,rating,sortPrice,pageNumber, dispatch])
 
     return (
         <div>
-          <Header/>
-          <section className='bg-[url("http://localhost:3001/images/banner/shop.png")] h-[220px] mt-6 bg-cover bg-no-repeat relative bg-left'>
+           <Header/>
+           <section className="bg-[url('http://localhost:3001/images/banner/shop.png')] h-[220px] mt-6 bg-cover bg-no-repeat relative bg-left">
             <div className='absolute left-0 top-0 w-full h-full bg-[#2422228a]'>
                 <div className='w-[85%] md:w-[80%] sm:w-[90%] lg:w-[90%] h-full mx-auto'>
                     <div className='flex flex-col justify-center gap-1 items-center h-full w-full text-white'>
-                <h2 className='text-3xl font-bold'>Shipping Page </h2>
+                <h2 className='text-3xl font-bold'>Shop Page </h2>
                 <div className='flex justify-center items-center gap-2 text-2xl w-full'>
                         <Link to='/'>Home</Link>
                         <span className='pt-1'>
                         <IoIosArrowForward />
                         </span>
-                        <span>Shipping </span>
+                        <span>Shop </span>
                       </div>
                     </div> 
                 </div> 
             </div> 
            </section>
 
+           <section className='py-16'>
+            <div className='w-[85%] md:w-[80%] sm:w-[90%] lg:w-[90%] h-full mx-auto'>
+            <div className={` md:block hidden ${!filter ? 'mb-6' : 'mb-0'} `}>
+                <button onClick={() => setFilter(!filter)} className='text-center w-full py-2 px-3 bg-orange-500 text-white'>Filter Product</button> 
+            </div>
 
-    <section className='bg-[#eeeeee]'>
-        <div className='w-[85%] lg:w-[90%] md:w-[90%] sm:w-[90%] mx-auto py-16'>
-           <div className='w-full flex flex-wrap'>
-            <div className='w-[67%] md-lg:w-full'>
-                <div className='flex flex-col gap-3'>
-                    <div className='bg-white p-6 shadow-sm rounded-md'>
-
-                        <h2 className='text-slate-600 font-bold pb-3'>Shipping Information </h2>
-
+            <div className='w-full flex flex-wrap'>
+                <div className={`w-3/12 md-lg:w-4/12 md:w-full pr-8 ${filter ? 'md:h-0 md:overflow-hidden md:mb-6' : 'md:h-auto md:overflow-auto md:mb-0' } `}>
+                    <h2 className='text-3xl font-bold mb-3 text-gray-900'>Category </h2>
+        <div className='py-2'>
             {
-              !res && <>
-             <form onSubmit={save}>
-            <div className='flex md:flex-col md:gap-2 w-full gap-5 text-gray-600'>
-            <div className='flex flex-col gap-1 mb-2 w-full'>
-                <label htmlFor="name"> Name </label>
-                <input onChange={inputHandle} value={state.name} type="text" className='w-full px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 transition-all' name="name" id="name" placeholder='Name' /> 
-            </div>
-
-            <div className='flex flex-col gap-1 mb-2 w-full'>
-                <label htmlFor="address"> Address </label>
-                <input onChange={inputHandle} value={state.address} type="text" className='w-full px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 transition-all' name="address" id="address" placeholder='Address' /> 
-            </div> 
-            </div>
-
-            <div className='flex md:flex-col md:gap-2 w-full gap-5 text-gray-600'>
-            <div className='flex flex-col gap-1 mb-2 w-full'>
-                <label htmlFor="phone"> Phone </label>
-                <input onChange={inputHandle} value={state.phone} type="text" className='w-full px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 transition-all' name="phone" id="phone" placeholder='Phone' /> 
-            </div>
-
-            <div className='flex flex-col gap-1 mb-2 w-full'>
-                <label htmlFor="post"> Post </label>
-                <input onChange={inputHandle} value={state.post} type="text" className='w-full px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 transition-all' name="post" id="post" placeholder='Post' /> 
-            </div> 
-            </div>
-
-            <div className='flex md:flex-col md:gap-2 w-full gap-5 text-gray-600'>
-            <div className='flex flex-col gap-1 mb-2 w-full'>
-                <label htmlFor="province"> Province </label>
-                <input onChange={inputHandle} value={state.province} type="text" className='w-full px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 transition-all' name="province" id="province" placeholder='Province' /> 
-            </div>
-
-            <div className='flex flex-col gap-1 mb-2 w-full'>
-                <label htmlFor="city"> City </label>
-                <input onChange={inputHandle} value={state.city} type="text" className='w-full px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 transition-all' name="city" id="city" placeholder='City' /> 
-            </div> 
-            </div>
-
-
-            <div className='flex md:flex-col md:gap-2 w-full gap-5 text-gray-600'>
-            <div className='flex flex-col gap-1 mb-2 w-full'>
-                <label htmlFor="area"> Area </label>
-                <input onChange={inputHandle} value={state.area} type="text" className='w-full px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 transition-all' name="area" id="area" placeholder='Area' /> 
-            </div>
-
-            <div className='flex flex-col gap-1 mt-7 mb-2 w-full'>
-               <button className='px-4 py-2 rounded-lg bg-blue-600 text-white font-semibold hover:bg-pink-700 hover:scale-105 hover:shadow-lg transition-all'>Save Change </button>
-            </div> 
-            </div> 
-                </form>
-
-                
-                </>
+                categorys.map((c,i) => <div key={i} className='flex justify-start items-center gap-2 py-1'>
+                    <input checked={category === c.name ? true : false} onChange={(e)=>queryCategory(e,c.name)} type="checkbox" id={c.name} />
+                    <label className='text-gray-900 block cursor-pointer' htmlFor={c.name}>{c.name}</label>
+                </div>)
             }
-
-            {
-                res && <div className='flex flex-col gap-1'>
-                <h2 className='text-gray-600 font-semibold pb-2'>Deliver To {state.name}</h2>
-                <p>
-                    <span className='bg-blue-200 text-blue-800 text-sm font-medium mr-2 px-2 py-1 rounded'>Home</span>
-                    <span>{state.phone} {state.address} {state.province} {state.city} {state.area}  </span>
-
-                    <span onClick={() => setRes(false)} className='text-indigo-500 cursor-pointer'>Change </span>
-                </p>
-
-                <p className='text-slate-600 text-sm' >Email To ariyan@gmail.com</p>
-
-            </div>
-            }
-              </div>
-
-              {
-                   products.map((p,i) => <div key={i} className='flex bg-white p-4 flex-col gap-2'>
-                   <div className='flex justify-start items-center'>
-                       <h2 className='text-md text-gray-600 font-bold'>{p.shopName}</h2>
-                   </div>
-
-                   {
-                       p.products.map((pt,i) => <div key={i} className='w-full flex flex-wrap'>
-                       <div className='flex sm:w-full gap-2 w-7/12'>
-                           <div className='flex gap-2 justify-start items-center'>
-                       <img className='w-[80px] h-[80px]' src={pt.productInfo.images[0]} alt="" />
-                       <div className='pr-4 text-gray-600'>
-                       <h2 className='text-md font-semibold'>{pt.productInfo.name} </h2>
-                       <span className='text-sm'>Brand: {pt.productInfo.brand}</span>
-                       </div>
-                           </div>
-                       </div>
-
-   <div className='flex justify-between w-5/12 sm:w-full sm:mt-3'>
-       <div className='pl-4 sm:pl-0'>
-       <h2 className='text-lg text-orange-500'>${pt.productInfo.price - Math.floor((pt.productInfo.price * pt.productInfo.discount) / 100)}</h2>
-           <p className='line-through'>${pt.productInfo.price}</p>
-           <p>-{pt.productInfo.discount}%</p>
-       </div>
-      
-   </div>
-
-
-                   </div>)
-                   }
-
-               </div>) 
-                } 
- 
-
-                </div> 
-            </div>
-
-            <div className='w-[33%] md-lg:w-full'>
-    <div className='pl-3 md-lg:pl-0 md-lg:mt-5'>
-        
-            <div className='bg-white p-3 text-slate-600 flex flex-col gap-3'>
-                <h2 className='text-xl font-bold'>Order Summary</h2>
-                <div className='flex justify-between items-center'>
-                    <span>Items Total (items) </span>
-                    <span>${price}</span>
-                </div>
-                <div className='flex justify-between items-center'>
-                    <span>Delivery Fee </span>
-                    <span>${shipping_fee} </span>
-                </div>
-
-                <div className='flex justify-between items-center'>
-                    <span>Total Payment </span>
-                    <span>${price + shipping_fee} </span>
-                </div>
-               
-
-                <div className='flex justify-between items-center'>
-                    <span>Total</span>
-                    <span className='text-lg text-[#059473]'>${price + shipping_fee} </span>
-                </div>
-                <button onClick={placeOrder} disabled={res ? false : true} className={`px-5 py-[6px] rounded-sm hover:shadow-red-500/50 hover:shadow-lg ${res ? 'bg-red-500' : 'bg-red-300'}  text-sm text-white uppercase`}>
-                   Place Order 
-                </button>
-
-            </div>
-        
-
-    </div>
-
-</div>
-
-
-
-            </div>  
-
         </div>
-        
-        
-   </section>       
 
-          <Footer/>
+        <div className='py-2 flex flex-col gap-5'>
+            <h2 className='text-3xl font-bold mb-3 text-gray-900'>Price</h2>
+             
+    <Range
+    step={5}
+    min={priceRange.low}
+    max={priceRange.high}
+    values={state.values}
+    onChange={(values) => setState({ values })}
+    renderTrack={({ props, children }) => {
+        return (
+            <div {...props} key="track" className="w-full h-[6px] bg-gray-300 rounded-full cursor-pointer">
+                {children}
+            </div>
+        );
+    }}
+    renderThumb={({ props, index }) => {
+        return (
+            <div {...props} key={`thumb-${index}`} className="w-[15px] h-[15px] bg-[#ff7f50] rounded-full" />
+        );
+    }}
+/>
+         <div>
+         <span className='text-slate-800 font-bold text-lg'>${Math.floor(state.values[0])} - ${Math.floor(state.values[1])}</span>  
+           </div>
+         </div>
+
+         <div className='py-3 flex flex-col gap-4'>
+            <h2 className='text-3xl font-bold mb-3 text-gray-900'>Rating </h2>
+            <div className='flex flex-col gap-3'>
+                 <div onClick={() => setRating(5)} className='text-orange-500 flex justify-start items-start gap-2 text-xl cursor-pointer'>
+                    <span><AiFillStar className=" w-6 h-6"/> </span>
+                    <span><AiFillStar/> </span>
+                    <span><AiFillStar/> </span>
+                    <span><AiFillStar/> </span>
+                    <span><AiFillStar/> </span>
+                  </div>
+
+                  <div onClick={() => setRating(4)} className='text-orange-500 flex justify-start items-start gap-2 text-xl cursor-pointer'>
+                    <span><AiFillStar/> </span>
+                    <span><AiFillStar/> </span>
+                    <span><AiFillStar/> </span>
+                    <span><AiFillStar/> </span>
+                    <span><CiStar/> </span>
+                  </div>
+
+                  <div onClick={() => setRating(3)} className='text-orange-500 flex justify-start items-start gap-2 text-xl cursor-pointer'>
+                    <span><AiFillStar/> </span>
+                    <span><AiFillStar/> </span>
+                    <span><AiFillStar/> </span>
+                    <span><CiStar/> </span>
+                    <span><CiStar/> </span>
+                  </div>
+
+                  <div onClick={() => setRating(2)} className='text-orange-500 flex justify-start items-start gap-2 text-xl cursor-pointer'>
+                    <span><AiFillStar/> </span>
+                    <span><AiFillStar/> </span>
+                    <span><CiStar/> </span>
+                    <span><CiStar/> </span>
+                    <span><CiStar/> </span>
+                  </div>
+
+                  <div onClick={() => setRating(1)} className='text-orange-500 flex justify-start items-start gap-2 text-xl cursor-pointer'>
+                    <span><AiFillStar/> </span>
+                    <span><CiStar/> </span>
+                    <span><CiStar/> </span>
+                    <span><CiStar/> </span>
+                    <span><CiStar/> </span>
+                  </div>
+
+                  <div className='text-orange-500 flex justify-start items-start gap-2 text-xl cursor-pointer'>
+                  <span><CiStar/> </span>
+                  <span><CiStar/> </span>
+                  <span><CiStar/> </span>
+                  <span><CiStar/> </span>
+                  <span><CiStar/> </span>
+                  </div> 
+            </div> 
+         </div>
+        
+        
+        <div className='py-5 flex flex-col gap-4 md:hidden'>
+            <Products title='Latest Product'  products={latest_product} />
+        </div> 
+          </div>
+
+        <div className='w-9/12 md-lg:w-8/12 md:w-full'>
+            <div className='pl-8 md:pl-0'>
+                <div className='py-4 bg-white mb-10 px-3 rounded-md flex justify-between items-start border'>
+                    <h2 className='text-lg font-medium text-blue-900'> ({totalProduct}) Products </h2>
+        <div className='flex justify-center items-center gap-3'>
+            <select onChange={(e)=>setSortPrice(e.target.value)} className='p-1 border outline-0 text-gray-900 font-semibold' name="" id="">
+                <option value="">Sort By</option>
+                <option value="low-to-high">Low to High Price</option>
+                <option value="high-to-low">High to Low Price </option>
+            </select>
+        <div className='flex justify-center items-start gap-4 md-lg:hidden'>
+            <div onClick={()=> setStyles('grid')} className={`p-2 ${styles === 'grid' && 'bg-gray-300'} text-gray-600 hover:bg-gray-300 cursor-pointer rounded-sm `} >
+                  <BsFillGridFill/>  
+            </div>
+            <div onClick={()=> setStyles('list')} className={`p-2 ${styles === 'list' && 'bg-gray-300'} text-gray-600 hover:bg-gray-300 cursor-pointer rounded-sm `} >
+                  <FaThList/>  
+            </div> 
+        </div> 
+        </div> 
+         </div> 
+
+         <div className='pb-8'>
+                  <ShopProducts products={products} styles={styles} />  
+         </div>
+
+         <div>
+           {
+             totalProduct > parPage &&  <Pagination pageNumber={pageNumber} setPageNumber={setPageNumber} totalItem={totalProduct} parPage={parPage} showItem={Math.floor(totalProduct / parPage )} />
+           }
+         </div>
+
+
+
+
+
+            </div> 
+         </div>  
+
+
+
+
+            </div>
+            </div> 
+           </section>
+
+           <Footer/>
         </div>
     );
 };
 
-export default Shipping;
+export default Shops;
