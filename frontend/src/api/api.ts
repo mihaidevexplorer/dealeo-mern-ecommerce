415a3c68e091e62d03a22dc59e087bd49c9a6e19
Mihai Tașu <146973331+mihaidevexplorer@users.noreply.github.com>
2025-12-09 19:27:19 +0200

    Set production API base URL for axios instance

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -2,9 +2,11 @@
 import axios from "axios";
 import type { AxiosInstance } from "axios";
 
+//const local = 'http://localhost:5000';
+const production = 'https://dealeo-backend.onrender.com';
 
 const api: AxiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
+    baseURL: `${production}/api`
 });
 
 export default api;
