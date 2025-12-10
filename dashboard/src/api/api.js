281d9939b96c9e4d88bd4e83e7e4f217001fd8d9
Mihai Tașu <146973331+mihaidevexplorer@users.noreply.github.com>
2025-12-10 19:19:40 +0200

    Update API base URL to use environment variable

--- dashboard/src/api/api.js
+++ dashboard/src/api/api.js
@@ -2,11 +2,10 @@
 import axios from "axios";
 import type { AxiosInstance } from "axios";
 
-//const local = 'http://localhost:5000';
-const production = 'https://dealeo-backend.onrender.com';
-
 const api: AxiosInstance = axios.create({
-    baseURL: `${production}/api`
+  baseURL: `${import.meta.env.VITE_DASHBOARD_API_URL}/api`,
+  
 });
 
 export default api;
+
