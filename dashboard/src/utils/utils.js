b090ef69088d8f3a3a9f1a8535ece65edb315290
Mihai Tașu <146973331+mihaidevexplorer@users.noreply.github.com>
2025-12-10 20:33:49 +0200

    Fix syntax and formatting in utils.js

--- dashboard/src/utils/utils.js
+++ dashboard/src/utils/utils.js
@@ -1,14 +1,15 @@
-import { io } from 'socket.io-client'
+import { io } from 'socket.io-client';
 
 export const overrideStyle = {
-    display : 'flex', 
-    margin : '0 auto',
-    height: '24px',
-    justifyContent : 'center',
-    alignItems : 'center'
-}
-
+  display: 'flex',
+  margin: '0 auto',
+  height: '24px',
+  justifyContent: 'center',
+  alignItems: 'center',
+};
 
 export const socket = io(import.meta.env.VITE_API_URL, {
-    transports: ['websocket'],
-    withCredentials: true        
+  transports: ['websocket'],
+  withCredentials: true,
+});
+   
