67b12523f5f374143c38ee04c50acb781594d81a
Mihai Tașu <146973331+mihaidevexplorer@users.noreply.github.com>
2025-12-10 17:59:20 +0200

    Update login and register links to use relative paths

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -106,13 +106,13 @@ const Login: React.FC = () => {
                                 <p>Don&apos;t Have An Account? <Link className='text-blue-500' to='/register'>Register</Link></p>
                             </div> 
 
-                            <a target='_blank' href="http://localhost:3001/login" rel="noreferrer">
+                            <a target='_blank' href="/login" rel="noreferrer">
                                 <div className='px-8 w-full py-2 bg-[#02e3e0] shadow hover:shadow-red-500/50 text-white rounded-md flex justify-center items-center gap-2 mb-3'>
                                     Login As a Seller
                                 </div>
                             </a>
          
-                            <a target='_blank' href="http://localhost:3001/register" rel="noreferrer">
+                            <a target='_blank' href="/register" rel="noreferrer">
                                 <div className='px-8 w-full py-2 bg-[#ad2cc4] shadow hover:shadow-red-500/50 text-white rounded-md flex justify-center items-center gap-2 mb-3'>
                                     Register As a Seller
                                 </div>
