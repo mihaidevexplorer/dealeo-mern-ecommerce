69364bb335ab86b4120cd7f8f83cd7a5c5811cf7
Mihai Tașu <146973331+mihaidevexplorer@users.noreply.github.com>
2025-12-10 19:49:39 +0200

    Refactor socket connection to use VITE_API_URL
    Updated socket connection to use environment variable for API URL.

--- dashboard/src/utils/utils.js
+++ dashboard/src/utils/utils.js
@@ -1,4 +1,5 @@
-import io from 'socket.io-client'
+import { io } from 'socket.io-client'
+
 export const overrideStyle = {
     display : 'flex', 
     margin : '0 auto',
@@ -6,4 +7,8 @@ export const overrideStyle = {
     justifyContent : 'center',
     alignItems : 'center'
 }
-export const socket = io('http://localhost:5000')
+
+// folosește variabila de mediu!!!
+export const socket = io(import.meta.env.VITE_API_URL, {
+    transports: ['websocket'],
+    withCredentials: true        
