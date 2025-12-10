367e94d109e637e68e93038de0d26b2c86aef860
Mihai Tașu <146973331+mihaidevexplorer@users.noreply.github.com>
2025-12-10 19:41:01 +0200

    Update image source paths to relative URLs

--- dashboard/src/views/seller/SellerToCustomer.jsx
+++ dashboard/src/views/seller/SellerToCustomer.jsx
@@ -100,7 +100,7 @@ const SellerToCustomer = () => {
         {
             customers.map((c,i) => <Link key={i} to={`/seller/dashboard/chat-customer/${c.fdId}`} className={`h-[60px] flex justify-start gap-2 items-center text-white px-2 py-2 rounded-md cursor-pointer bg-red-500`}>
             <div className='relative'>
-             <img className='w-[38px] h-[38px] border-white border-2 max-w-[38px] p-[2px] rounded-full' src="http://localhost:3000/images/admin.jpg" alt="" />
+             <img className='w-[38px] h-[38px] border-white border-2 max-w-[38px] p-[2px] rounded-full' src="/images/admin.jpg" alt="" />
              <div className='w-[10px] h-[10px] bg-green-500 rounded-full absolute right-0 bottom-0'></div>
             </div>
     
@@ -128,7 +128,7 @@ const SellerToCustomer = () => {
             {
                 sellerId && <div className='flex justify-start items-center gap-3'>
            <div className='relative'>
-         <img className='w-[45px] h-[45px] border-green-500 border-2 max-w-[45px] p-[2px] rounded-full' src="http://localhost:3000/images/demo.jpg" alt="" />
+         <img className='w-[45px] h-[45px] border-green-500 border-2 max-w-[45px] p-[2px] rounded-full' src="/images/demo.jpg" alt="" />
          <div className='w-[10px] h-[10px] bg-green-500 rounded-full absolute right-0 bottom-0'></div>
         </div>
         <h2 className='text-base text-white font-semibold'>{currentCustomer.name}</h2>
@@ -151,7 +151,7 @@ const SellerToCustomer = () => {
                     <div key={i} ref={scrollRef} className='w-full flex justify-start items-center'>
                     <div className='flex justify-start items-start gap-2 md:px-3 py-2 max-w-full lg:max-w-[85%]'>
                         <div>
-                            <img className='w-[38px] h-[38px] border-2 border-white rounded-full max-w-[38px] p-[3px]' src="http://localhost:3000/images/demo.jpg" alt="" />
+                            <img className='w-[38px] h-[38px] border-2 border-white rounded-full max-w-[38px] p-[3px]' src="/images/demo.jpg" alt="" />
                         </div>
                         <div className='flex justify-center items-start flex-col w-full bg-purple-500 shadow-lg shadow-red-500/50 text-white py-1 px-2 rounded-sm'>
                         <span>{m.message} </span>
@@ -168,7 +168,7 @@ const SellerToCustomer = () => {
                         <span>{m.message} </span>
                         </div> 
                         <div>
-                            <img className='w-[38px] h-[38px] border-2 border-white rounded-full max-w-[38px] p-[3px]' src="http://localhost:3000/images/admin.jpg" alt="" />
+                            <img className='w-[38px] h-[38px] border-2 border-white rounded-full max-w-[38px] p-[3px]' src="/images/admin.jpg" alt="" />
                         </div>
 
                     </div> 
