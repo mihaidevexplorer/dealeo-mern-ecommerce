4d529b2483d248d852ef8d78e1aec75a1e9122a7
Mihai Tașu <146973331+mihaidevexplorer@users.noreply.github.com>
2025-12-10 19:39:44 +0200

    Update image source paths to relative URLs

--- dashboard/src/views/seller/SellerToAdmin.jsx
+++ dashboard/src/views/seller/SellerToAdmin.jsx
@@ -72,7 +72,7 @@ const SellerToAdmin = () => {
         <div className='flex justify-between items-center'>
             <div className='flex justify-start items-center gap-3'>
            <div className='relative'>
-         <img className='w-[45px] h-[45px] border-green-600 border-2 max-w-[45px] p-[2px] rounded-full' src="http://localhost:3000/images/demo.jpg" alt="" />
+         <img className='w-[45px] h-[45px] border-green-600 border-2 max-w-[45px] p-[2px] rounded-full' src="/images/demo.jpg" alt="" />
          <div className='w-[10px] h-[10px] bg-green-600 rounded-full absolute right-0 bottom-0'></div>
         </div>
         <h2 className='text-base text-white font-semibold'>Support</h2>
@@ -91,7 +91,7 @@ const SellerToAdmin = () => {
 <div ref={scrollRef} key={i} className='w-full flex justify-start items-center'>
         <div className='flex justify-start items-start gap-2 md:px-3 py-2 max-w-full lg:max-w-[85%]'>
             <div>
-                <img className='w-[38px] h-[38px] border-2 border-white rounded-full max-w-[38px] p-[3px]' src="http://localhost:3000/images/demo.jpg" alt="" />
+                <img className='w-[38px] h-[38px] border-2 border-white rounded-full max-w-[38px] p-[3px]' src="/images/demo.jpg" alt="" />
             </div>
             <div className='flex justify-center items-start flex-col w-full bg-purple-500 shadow-lg shadow-purple-500/50 text-white py-1 px-2 rounded-sm'>
             <span>{m.message} </span>
@@ -109,7 +109,7 @@ const SellerToAdmin = () => {
                         <span>{m.message}  </span>
                         </div> 
                         <div>
-                            <img className='w-[38px] h-[38px] border-2 border-white rounded-full max-w-[38px] p-[3px]' src="http://localhost:3000/images/admin.jpg" alt="" />
+                            <img className='w-[38px] h-[38px] border-2 border-white rounded-full max-w-[38px] p-[3px]' src="/images/admin.jpg" alt="" />
                         </div>
 
                     </div> 
