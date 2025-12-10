72b354a7ff96cf4838314d67af60dfff06234921
Mihai Tașu <146973331+mihaidevexplorer@users.noreply.github.com>
2025-12-10 17:39:00 +0200

    Update logo image source in Footer component

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -20,7 +20,7 @@ const Footer: React.FC = () => {
             <div className='w-[85%] flex flex-wrap mx-auto border-b py-16 md-lg:pb-10 sm:pb-6'>
                 <div className='w-3/12 lg:w-4/12 sm:w-full'>
                     <div className='flex flex-col gap-3'>
-                        <img className='w-[190px] h-[70px]' src="http://localhost:3001/images/logo.png" alt="logo" />
+                        <img className='w-[190px] h-[70px]' src="/images/logo.png" alt="logo" />
                         <ul className='flex flex-col gap-2 text-gray-600'>
                             <li>Address :  2504 Ivins Avenue, Egg Harbor Township, NJ 08234,</li>
                             <li>Phone : 4343434344</li>
