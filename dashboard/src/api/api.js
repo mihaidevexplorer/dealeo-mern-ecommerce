7122976bca8feb0438846e525e42dc9a6b4b84ce
Mihai Tașu <146973331+mihaidevexplorer@users.noreply.github.com>
2025-12-10 19:28:20 +0200

    Update API base URL in api.js

--- dashboard/src/api/api.js
+++ dashboard/src/api/api.js
@@ -1,10 +1,7 @@
-// src/api/api.ts
 import axios from "axios";
-import type { AxiosInstance } from "axios";
 
-const api: AxiosInstance = axios.create({
-  baseURL: `${import.meta.env.VITE_DASHBOARD_API_URL}/api`,
-  
+const api = axios.create({
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
 });
 
 export default api;
