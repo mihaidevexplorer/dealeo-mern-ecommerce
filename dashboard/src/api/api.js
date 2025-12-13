07a2e47a3d0bfd47871b115d75536c323bed0342
Mihai Tașu <146973331+mihaidevexplorer@users.noreply.github.com>
2025-12-13 22:53:43 +0200

    Update API base URL to use VITE_API_URL

--- dashboard/src/api/api.js
+++ dashboard/src/api/api.js
@@ -1,19 +1,13 @@
 // src/api/api.ts
 import axios from "axios";
 
-//const local = "http://localhost:5000";
-
-
-const production = "https://dealeo-backend.onrender.com";
-
-const api_url = import.meta.env.PROD ? production : local;
-
 const api = axios.create({
-  baseURL: `${api_url}/api`,
-  withCredentials: true,         
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
+  withCredentials: true,
 });
 
 export default api;
+
 
 
 
