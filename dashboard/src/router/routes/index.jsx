cb0520d27d6ee5e57ba48259a17ad7d3352fcd17
Mihai Tașu <146973331+mihaidevexplorer@users.noreply.github.com>
2025-12-14 20:52:52 +0200

    Update index.jsx

--- dashboard/src/router/routes/index.jsx
+++ dashboard/src/router/routes/index.jsx
@@ -4,15 +4,16 @@ import MainLayout from './../../layout/MainLayout';
 import ProtectRoute from './ProtectRoute';
 
 export const getRoutes = () => {
- 
-    const protectedRoutes = privateRoutes.map(r => ({
-        ...r,
-        element: <ProtectRoute route={r}>{r.element}</ProtectRoute>
-    }));
+
+    privateRoutes.map(r => {
+        
+       r.element = <ProtectRoute route={r} >{r.element}</ProtectRoute>
+    })
+
 
     return {
-        path: '/',
-        element: <MainLayout />,
-        children: protectedRoutes
+        path : '/',
+        element : <MainLayout />,
+        children : privateRoutes
     }
 }
