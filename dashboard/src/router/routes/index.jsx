037bba0efb716d717607930c9f349632b5f36292
Mihai Tașu <146973331+mihaidevexplorer@users.noreply.github.com>
2025-12-14 20:42:53 +0200

    Update index.jsx

--- dashboard/src/router/routes/index.jsx
+++ dashboard/src/router/routes/index.jsx
@@ -1,20 +1,18 @@
+//src/router/routes/index.js
 import { privateRoutes } from './privateRoutes';
 import MainLayout from './../../layout/MainLayout';
 import ProtectRoute from './ProtectRoute';
-import { Navigate } from 'react-router-dom';
 
 export const getRoutes = () => {
-  const protectedRoutes = privateRoutes.map(route => ({
-    ...route,
-    element: <ProtectRoute route={route}>{route.element}</ProtectRoute>
-  }));
+ 
+    const protectedRoutes = privateRoutes.map(r => ({
+        ...r,
+        element: <ProtectRoute route={r}>{r.element}</ProtectRoute>
+    }));
 
-  return {
-    path: '/',
-    element: <MainLayout />,
-    children: [
-      ...protectedRoutes,
-      { path: '*', element: <Navigate to="/" replace /> }
-    ]
-  };
-};
+    return {
+        path: '/',
+        element: <MainLayout />,
+        children: protectedRoutes
+    }
+}
