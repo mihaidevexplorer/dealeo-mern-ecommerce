7c374dfc4b4ccb872510f8f2c625358144f7944a
Mihai Tașu <146973331+mihaidevexplorer@users.noreply.github.com>
2025-12-14 19:40:31 +0200

    Update main.jsx

--- dashboard/src/main.jsx
+++ dashboard/src/main.jsx
@@ -1,28 +1,30 @@
 // dashboard/src/main.jsx
-import { Suspense } from 'react';
-import ReactDOM from 'react-dom/client';
+import {lazy,Suspense} from 'react';
+import ReactDOM from 'react-dom/client'; 
 import './index.css';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store/index';
 import { Toaster } from 'react-hot-toast';
-import { getRoutes } from './router/routes';
+
+const App = lazy(() => import('./App'))
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-const router = createBrowserRouter([getRoutes()]);
-
 root.render(
-  <Provider store={store}>
+  <BrowserRouter>
+   <Provider store={store} >
     <Suspense>
-      <RouterProvider router={router} />
-      <Toaster
-        toastOptions={{
-          position: 'top-right',
-          style: { background: '#283046', color: 'white' },
-        }}
-      />
+    <App /> 
+    <Toaster
+      toastOptions={{
+        position : 'top-right',
+        style : {
+          background : '#283046',
+          color : 'white'
+        }
+      }} 
+    />
     </Suspense>
-  </Provider>
+    </Provider>
+  </BrowserRouter>
 );
-
