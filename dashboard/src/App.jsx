81a61b04e5325779f36f4b3edfb07d76030a0df9
Mihai Tașu <146973331+mihaidevexplorer@users.noreply.github.com>
2025-12-14 19:47:33 +0200

    Update App.jsx

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -1,33 +1,22 @@
 //src/App.jsx
-import { useEffect, useState } from "react";
 import Router from "./router/Router";
 import publicRoutes from "./router/routes/publicRoutes";
 import { getRoutes } from "./router/routes";
 import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
 import { get_user_info } from "./store/Reducers/authReducer";
 
 function App() {
+  const dispatch = useDispatch();
+  const { token } = useSelector(state => state.auth);
 
-    const dispatch = useDispatch()
-    const { token } = useSelector(state => state.auth)
+  useEffect(() => {
+    if (token) dispatch(get_user_info());
+  }, [token, dispatch]);
 
-    const [allRoutes, setAllRoutes] = useState([...publicRoutes])
-    // console.log(allRoutes)
+  const allRoutes = [...publicRoutes, getRoutes()];
 
-    useEffect(() => {
-        const routes = getRoutes()
-        setAllRoutes([...allRoutes,routes])
-    },[])
-
-    useEffect(() => {
-        if (token) {
-            dispatch(get_user_info())
-        }
-
-    },[token])
-
-
-    return <Router allRoutes={allRoutes} /> 
+  return <Router allRoutes={allRoutes} />;
 }
 
 export default App;
