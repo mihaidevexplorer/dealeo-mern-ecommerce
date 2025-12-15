150bfd8b2e0ef5901cee6d8b484b07895b7bec74
Mihai Tașu <146973331+mihaidevexplorer@users.noreply.github.com>
2025-12-15 21:38:09 +0200

    Update ProtectRoute.jsx

--- dashboard/src/router/routes/ProtectRoute.jsx
+++ dashboard/src/router/routes/ProtectRoute.jsx
@@ -1,5 +1,4 @@
 //src/router/routes/ProtectRoute.js
-//src/router/routes/ProtectRoute.js
 import { Suspense } from 'react';
 import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
