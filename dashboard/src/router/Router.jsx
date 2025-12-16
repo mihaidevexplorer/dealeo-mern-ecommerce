bcd4d476a794a0c20460948f8e2fd0a7c8369262
Mihai Tașu <146973331+mihaidevexplorer@users.noreply.github.com>
2025-12-16 19:50:17 +0200

    Update Router.jsx

--- dashboard/src/router/Router.jsx
+++ dashboard/src/router/Router.jsx
@@ -1,15 +1,15 @@
 //src/router/Router.jsx
-
-import { useRoutes, Navigate } from 'react-router-dom';
+import { useRoutes, Navigate } from "react-router-dom";
 
 const Router = ({ allRoutes }) => {
   const routes = useRoutes([
     ...allRoutes,
-    { path: '*', element: <Navigate to="/login" replace /> }
+    { path: "*", element: <Navigate to="/" replace /> }
   ]);
 
   return routes;
 };
 
 export default Router;
+
 
