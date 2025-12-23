b79790e26a8feee5b02e5eb3ed7aae52be1ae409
Mihai Tașu <146973331+mihaidevexplorer@users.noreply.github.com>
2025-12-23 20:57:30 +0200

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,28 +1,31 @@
+//tailwind.config.js
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       fontFamily: {
-        roboto: ["Roboto", "sans-serif"],
-      },
-      screens: {
-        "3xl": "1920px",
-        "4xl": "2560px",
-
-        // max-width helpers (NU suprascriu sm/md/lg/xl)
-        "max-xl": { max: "1200px" },
-        "max-lg": { max: "1080px" },
-        "max-mdlg": { max: "991px" },
-        "max-md": { max: "768px" },
-        "max-sm": { max: "576px" },
-        "max-xs": { max: "480px" },
-        "max-2xs": { max: "340px" },
-      },
-    },
+        roboto: ['Roboto', 'sans-serif'],
+    },},
+    screens: {
+      'xl': {'max': '1200px'},
+      'lg': {'max': '1080px'},
+      'md-lg': {'max': '991px'},
+      'md': {'max': '768px'},
+      'sm': {'max': '576px'},
+      'xs': {'max': '480px'},
+      '2xs': {'max': '340px'},
+    }
   },
   plugins: [],
-};
+}
+
+
+
 
 
 
